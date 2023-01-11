ac9c2c0ca5b639ba28b46b2d07bffb27000b9865
lewh98 <lewishalstead.test@gmail.com>
2023-01-11 13:16:35 +0000

    Added asnwers for ex.d q4&5 from hw solution

--- exercise_d.py
+++ exercise_d.py
@@ -21,34 +21,30 @@ for x in range(0, len(numbers)):
 #    BUT ignore any section of numbers starting with a 6 and extending to the next 7.
 #    So [11, 6, 4, 99, 7, 11] would have sum of 22
 
-for x in range(0, len(numbers)):
-    if numbers[x] == numbers[] and numbers[x] == 2 and numbers[x-1] ==2:
-        # print(True)
-        pass
+total = 0
+found_6 = False
+for number in numbers:
+    if number == 6:
+        found_6 = True
+    elif found_6:
+        if number == 7:
+            found_6 = False
+    else:
+        total += number
+
+print(total)
 
 
-
-
-
-
-
-
-
-
-
-
+# for x in range(0, len(numbers)):
+#     if numbers[x] == numbers[] and numbers[x] == 2 and numbers[x-1] ==2:
+#         # print(True)
+#         pass
 
 
 # index = 0
 # for x in numbers:
 #     while x != 6 
-
-
 # total = sum(numbers)
-
-
-
-
 
 
 # x = 0
@@ -72,11 +68,6 @@ for x in range(0, len(numbers)):
 
 
 
-
-
-
-
-
 # 5. HARD! Print the sum of the numbers. 
 #    Except the number 13 is very unlucky, so it does not count.
 #    And numbers that come immediately after a 13 also do not count.
@@ -84,3 +75,13 @@ for x in range(0, len(numbers)):
 #
 #    So [5, 13, 2] would have sum of 5. 
 
+
+index = 0
+total = 0
+for number in numbers:
+    if number == 13 or numbers[index-1] == 13:
+        pass
+    else:
+         total += number
+         index += 1
+print(total)
