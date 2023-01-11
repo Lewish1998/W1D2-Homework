1ed7705ec7e88290179bf077e1d9a90dd98f1c9f
lewh98 <lewishalstead.test@gmail.com>
2023-01-11 10:01:52 +0000

    Reformatted ex.a

--- exercise_a.py
+++ exercise_a.py
@@ -1,32 +1,40 @@
 #1. Add "Edinburgh Waverley" to the end of the list
-#2. Add "Glasgow Queen St" to the start of the list
-#3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
-#4. Print out the index position of "Linlithgow"
-#5. Remove "Livingston" from the list using its name
-#6. Delete "Cumbernauld" from the list by index
-#7. Print the number of stops there are in the list
-#8. Sort the list alphabetically
-#9. Reverse the positions of the stops in the list
-#10 Print out all the stops using a for loop
-
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+print(stops)
 
+#2. Add "Glasgow Queen St" to the start of the list
 stops.append('Edinburgh Waverly')
+print(stops)
 
+#3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
 stops.insert(0, 'Glasgow Queen Street')
+print(stops)
 
+#4. Print out the index position of "Linlithgow"
 stops.insert(4, 'Polmont')
+print(stops)
 
+#5. Remove "Livingston" from the list using its name
 print(stops.index('Linlithgow'))
+print(stops)
 
+#6. Delete "Cumbernauld" from the list by index
 stops.remove('Livingston')
+print(stops)
 
+#7. Print the number of stops there are in the list
 del stops[2]
+print(stops)
 
+#8. Sort the list alphabetically
 stops.sort()
+print(stops)
 
+#9. Reverse the positions of the stops in the list
 stops.reverse()
+print(stops)
 
+#10 Print out all the stops using a for loop
 for stop in stops:
     print(stop)
 
--- exercise_d.py
+++ exercise_d.py
@@ -14,11 +14,22 @@ difference = max(numbers) - min(numbers)
 # 3. Print True if the list contains a 2 next to a 2 somewhere.
 for x in range(0, len(numbers)):
     if numbers[x] == numbers[x-1] and numbers[x] == 2 and numbers[x-1] ==2:
-        print(True)
+        # print(True)
+        pass
 
 # 4. Print the sum of the numbers, 
 #    BUT ignore any section of numbers starting with a 6 and extending to the next 7.
 #    So [11, 6, 4, 99, 7, 11] would have sum of 22
+
+# loop through list of numbers
+# find the number 6 by checking number with index ?
+# don't do anything until the same process to find 7 is done
+# continue adding and repeat ^
+
+for x in numbers:
+    if x == 6:
+        numbers[]
+
 
 # 5. HARD! Print the sum of the numbers. 
 #    Except the number 13 is very unlucky, so it does not count.
